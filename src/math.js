658e12c8a949cce0da93a7ae6ab0be7a3aa8eaf2
3a@hw <arbuzov.aa@inbox.ru>
2024-09-16 11:58:02 +0300

    add src/math.js

---
+++ src/math.js
@@ -0,0 +1,8 @@
+// my math functions
+
+// return rundom num from min to max
+export default (min, max) => {
+  // случайное число от min до (max+1)
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
